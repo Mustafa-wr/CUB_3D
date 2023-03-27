6d7591c739124b8389805d330d2bc05486adbea0
basheer laptop <basheer753@gmail.com>
2023-03-27 22:29:19 +0400

    added sync

--- include/defines.h
+++ include/defines.h
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 01:52:39 by bammar            #+#    #+#             */
-/*   Updated: 2023/03/27 02:00:40 by bammar           ###   ########.fr       */
+/*   Updated: 2023/03/27 21:46:08 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # define PNAME "cub3d"
 # define PI 3.141593
 # define PLAYERSPEED 10
+# define FPS 60
 
 #endif
--- src/hooks.c
+++ src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 03:00:24 by bammar            #+#    #+#             */
-/*   Updated: 2023/03/27 21:15:40 by bammar           ###   ########.fr       */
+/*   Updated: 2023/03/27 21:53:01 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int temp_exit(t_hook_vars *hook_vars)
 {
 	mlx_clear_window(hook_vars->mlx_vars->mlx_ptr,
 		hook_vars->mlx_vars->win_ptr);
+	mlx_destroy_image(hook_vars->mlx_vars->mlx_ptr,
+		hook_vars->mlx_vars->main_img);
+	mlx_destroy_window(hook_vars->mlx_vars->mlx_ptr,
+		hook_vars->mlx_vars->win_ptr);
+	ht_destroy(hook_vars->map);
 	exit(0);
 	return 0;
 }
@@ -24,11 +29,12 @@ int	key_hook(int keycode, t_hook_vars *hook_vars)
 {
     t_vec *player;
 
-    player = hook_vars->player;
+    mlx_do_sync(hook_vars->mlx_vars->mlx_ptr);
+	player = hook_vars->player;
 	if (keycode == ESC)
 		temp_exit(hook_vars);
     move_by_key(keycode, player);
 	printf("(%.02f,%.02f), angle: %.02f\n", player->p.x, player->p.y, player->angle);
-    draw2d(hook_vars);
+	draw2d(hook_vars);
 	return (0);
 }
