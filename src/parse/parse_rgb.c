53d1bab79d4d57a9ab1fe10e95d7093dddde318d
must <mradwan@student.42abudhabi.ae>
2023-05-11 13:48:05 +0400

    fixed leaks

--- src/parse/parse_rgb.c
+++ src/parse/parse_rgb.c
@@ -6,7 +6,7 @@
 /*   By: mradwan <mradwan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 17:39:33 by mradwan           #+#    #+#             */
-/*   Updated: 2023/05/07 19:56:48 by mradwan          ###   ########.fr       */
+/*   Updated: 2023/05/11 13:47:28 by mradwan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,16 +104,14 @@ int	store_the_rgb(t_cub3d *map)
 		return (free_strings(map->map), free(map->floor), free(map->cieling), \
 			free_tools(map), ft_putendl_fd("Error", 2), 0);
 	v.i = -1;
-	while (++v.i < 3){
+	while (++v.i < 3)
 		if (map->cieling[v.i] > 255 || map->cieling[v.i] < 0)
-		{
-			return (free_strings(map->map), ft_putendl_fd("Error", 2), 0);
-		}printf("f%d\n",  map->cieling[v.i]);}
+			return (free_strings(map->map), free_tools(map), free(map->floor), \
+				free(map->cieling), ft_putendl_fd("Error", 2), 0);
 	v.i = -1;
-	while (++v.i < 3){
+	while (++v.i < 3)
 		if (map->floor[v.i] > 255 || map->floor[v.i] < 0)
-		{
-			return (free_strings(map->map), ft_putendl_fd("Error", 2), 0);
-		}printf("f%d\n",  map->floor[v.i]);}
+			return (free_strings(map->map), free_tools(map), free(map->floor), \
+				free(map->cieling), ft_putendl_fd("Error", 2), 0);
 	return (1);
 }
